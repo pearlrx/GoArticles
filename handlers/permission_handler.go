bad53c9793857b9bc7b8e6d73c1908a79e07fa12
pearlweb <dima_kud2005@mail.ru>
2024-08-06 13:33:42 +0300

    permissions commit

--- handlers/permission_handler.go
+++ handlers/permission_handler.go
@@ -1,1 +1,84 @@
 package handlers
+
+import (
+	"GoArticles/models"
+	"database/sql"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
+
+type PermissionHandler struct {
+	DB *sql.DB
+}
+
+func NewPermissionHandler(db *sql.DB) *PermissionHandler {
+	return &PermissionHandler{DB: db}
+}
+
+func (h *PermissionHandler) AssignPermissionToRole(c echo.Context) error {
+	roleID, err := strconv.Atoi(c.Param("role_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]string{"error": "Invalid role id"})
+	}
+
+	permissionID, err := strconv.Atoi(c.Param("permission_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]string{"error": "Invalid permission id"})
+	}
+
+	query := `INSERT INTO role_permissions(role_id, permission_id) VALUES ($1, $2)`
+	_, err = h.DB.ExecContext(c.Request().Context(), query, roleID, permissionID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, map[string]string{"error": "Failed to assigned permission to role"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Role assigned successfully"})
+}
+
+func (h *PermissionHandler) RemovePermissionFromRole(c echo.Context) error {
+	roleID, err := strconv.Atoi(c.Param("role_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]string{"error": "Invalid role id"})
+	}
+
+	permissionID, err := strconv.Atoi(c.Param("permission_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]string{"error": "Invalid permission id"})
+	}
+
+	query := `DELETE FROM role_permissions WHERE role_id = $1 AND permission_id = $2`
+	_, err = h.DB.ExecContext(c.Request().Context(), query, roleID, permissionID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, map[string]string{"error": "Failed to remove permission from role"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Permission remove successfully"})
+}
+
+func (h *PermissionHandler) GetPermissionsByRole(c echo.Context) error {
+	roleID, err := strconv.Atoi(c.Param("role_id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid role ID"})
+	}
+
+	query := `SELECT p.id, p.name FROM permissions p
+              JOIN role_permissions rp ON p.id = rp.permission_id
+              WHERE rp.role_id = $1`
+	rows, err := h.DB.QueryContext(c.Request().Context(), query, roleID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve permissions"})
+	}
+	defer rows.Close()
+
+	var permissions []models.Permission
+	for rows.Next() {
+		var permission models.Permission
+		if err = rows.Scan(&permission.ID, &permission.Name); err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to scan permission"})
+		}
+		permissions = append(permissions, permission)
+	}
+
+	return c.JSON(http.StatusOK, permissions)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -10,50 +10,47 @@ func InitRoutes(e *echo.Echo, db *sql.DB) {
 	userHandler := handlers.NewUserHandler(db)
 	articleHandler := handlers.NewArticleHandler(db)
 	roleHandler := handlers.NewRoleHandler(db)
+	permissionHandler := handlers.NewPermissionHandler(db)
+
 	// Get user by id
 	e.GET("/users/:id", userHandler.GetUserByID)
-
 	// Get user role by id
 	e.GET("/users/:id/roles", userHandler.GetUserRoles)
-
 	// Get user settings by ID
 	e.GET("/users/:id/settings", userHandler.GetUserSettings)
-
 	// Creating new user
 	e.POST("/users", userHandler.CreateUser)
-
 	// Removing a user by id
 	e.DELETE("/users/:id", userHandler.DeleteUser)
-
 	// Get article by ID
 	e.GET("/articles/:id", articleHandler.GetArticleByID)
 
 	// Create a new article
 	e.POST("/articles", articleHandler.CreateArticle)
-
 	// Update existing article by ID
 	e.PUT("/articles/:id", articleHandler.UpdateArticle)
-
 	// Delete article by ID
 	e.DELETE("/articles/:id", articleHandler.DeleteArticle)
-
 	// Add a category to an article
 	e.POST("/articles/:article_id/categories/:category_id", articleHandler.AddArticleCategory)
-
 	// Add a tag to an article
 	e.POST("/articles/:article_id/tags/:tag_id", articleHandler.AddArticleTag)
-
 	// Like an article by user ID
 	e.POST("/articles/:article_id/like/:user_id", articleHandler.LikeArticle)
 
 	//TODO: adding roles and permissions to users
 
-	// get role
+	// Get role
 	e.GET("/roles", roleHandler.GetRoles)
-
-	// add role to user
+	// Add role to user
 	e.PUT("/users/:user_id/roles/:role_id", roleHandler.AssignRoleToUser)
-
-	// delete role from user
+	// Delete role from user
 	e.DELETE("/users/:user_id/roles/:role_id", roleHandler.RemoveRoleFromUser)
+
+	// Add permission to role
+	e.PUT("/roles/:role_id/permissions/:permission_id", permissionHandler.AssignPermissionToRole)
+	// Delete permission from role
+	e.DELETE("/roles/:role_id/permissions/:permission_id", permissionHandler.RemovePermissionFromRole)
+	// Get permission
+	e.GET("/roles/:role_id/permissions", permissionHandler.GetPermissionsByRole)
 }
